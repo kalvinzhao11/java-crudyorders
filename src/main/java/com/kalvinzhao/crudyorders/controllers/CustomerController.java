7415200d6f2bc28dc46caad6db85f3ef79365297
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-10-07 23:14:36 -0400

    added controllers for customer

--- src/main/java/com/kalvinzhao/crudyorders/controllers/CustomerController.java
+++ src/main/java/com/kalvinzhao/crudyorders/controllers/CustomerController.java
@@ -3,13 +3,14 @@ package com.kalvinzhao.crudyorders.controllers;
 import com.kalvinzhao.crudyorders.models.Customer;
 import com.kalvinzhao.crudyorders.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -54,5 +55,35 @@ public class CustomerController {
 //
 //    DELETE /customers/customer/{custcode} - Deletes the given customer including any associated orders
 
+    @PostMapping(value = "/customer", produces = {"application/json"} , consumes = {"application/json"})
+    public ResponseEntity<?> addCustomer(@Valid @RequestBody Customer newCustomer){
+        newCustomer.setCustcode(0); //setting id = 0 means that it's a post request
+        newCustomer = customerServices.save(newCustomer);
 
+        HttpHeaders responseHeader = new HttpHeaders();
+        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{custid}")
+                .buildAndExpand(newCustomer.getCustcode())
+                .toUri();
+        responseHeader.setLocation(newCustomerURI);
+        return new ResponseEntity<>(newCustomer, responseHeader, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/customer/{custcode}", produces = {"application/json"} , consumes = {"application/json"})
+    public ResponseEntity<?> replaceCustomer(@PathVariable long custcode, @Valid @RequestBody Customer updateCustomer){
+        updateCustomer.setCustcode(custcode);
+        updateCustomer = customerServices.save(updateCustomer);
+        return new ResponseEntity<>(updateCustomer, HttpStatus.OK);
+    }
+    @PatchMapping(value = "/customer/{custcode}", consumes = {"application/json"})
+    public ResponseEntity<?> updateCustomerByCustcode(@PathVariable long custcode, @RequestBody Customer updateCustomer){
+        customerServices.update(updateCustomer, custcode);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/customers/customer/{custcode}")
+    public ResponseEntity<?> deleteCustomerByCustcode(@PathVariable long custcode){
+        customerServices.delete(custcode);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
